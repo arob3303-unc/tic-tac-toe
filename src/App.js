20391477fcf4e00f0bea729287b7a5860587fb37
arob3303-unc <arob3303@unc.edu>
2025-07-14 02:26:21 -0400

    board component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+ 
+// square function -> to create a square
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  // onClick uses the function above ^
+  return <button className="square" onClick={handleClick}>{value}</button>;
+}
+
+// tic tac toe board
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
 }

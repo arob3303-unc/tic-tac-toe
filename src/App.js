26323124e53d9f7aad3e11ade1bf82501a1f2afe
arob3303-unc <arob3303@unc.edu>
2025-07-16 00:34:25 -0400

    made a game top-component. Board component feeds into. Allows history to be read more easily, so the player knows what moves lead to the current move.

--- src/App.js
+++ src/App.js
@@ -5,24 +5,70 @@ function Square({value, onSquareClick}) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
+// function to find winner
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 // tic tac toe board
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null))
+function Board({xIsNext, squares, onPlay}) {
+  const winner = calculateWinner(squares);
+  let status;
+  let int = 0;
+
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] === null) {
+      int += 1;
+    }
+  }
+
+  if (winner) {
+    status = "Winner: " + winner;
+  } else if (int === 9) {
+    status = "Start of the game! X is first!";
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
 
   function handleClick(i) {
+    // check winner
+    if (calculateWinner(squares)) {
+      return;
+    }
+
+    if (squares[i] != null) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares);
   }
 
   return (
     <>
+      {/* display winner or turn */}
+      <div className="status">{status}</div>
+
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -39,5 +85,27 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{/* TODO */ }</ol>
+      </div>
+    </div>
   );
 }
